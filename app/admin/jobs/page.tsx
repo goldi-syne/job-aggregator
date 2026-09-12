decd188086f19c1eb5912b638fa64354b7e4c658
goldi-syne <goldichatgpt@gmail.com>
2026-09-13 04:55:54 +0530

    Show inactive jobs cleanup button in admin

--- app/admin/jobs/page.tsx
+++ app/admin/jobs/page.tsx
@@ -1,8 +1,12 @@
 import { redirect } from 'next/navigation';
 import { isAdminAuthenticated } from '@/lib/admin-auth';
 import AdminJobsClient from './AdminJobsClient';
+import DeleteInactiveButton from './DeleteInactiveButton';
 
 export default async function AdminJobsPage() {
   if (!(await isAdminAuthenticated())) redirect('/admin/login');
-  return <AdminJobsClient />;
+  return <>
+    <DeleteInactiveButton />
+    <AdminJobsClient />
+  </>;
 }
