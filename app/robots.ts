88824a8da7b354b61571f58d78aa9f0ea05f5ce8
goldi-syne <goldichatgpt@gmail.com>
2026-09-12 21:28:51 +0530

    Tighten robots crawl rules

--- app/robots.ts
+++ app/robots.ts
@@ -3,7 +3,13 @@ import { absoluteUrl } from '@/lib/site';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [{ userAgent: '*', allow: '/', disallow: ['/api/', '/go/'] }],
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/go/', '/admin/'],
+      },
+    ],
     sitemap: absoluteUrl('/sitemap.xml'),
   };
 }
